c5e1d3b0a0546c177eedb4cc6404566c1ca4f840
PGN <21120529@student.hcmus.edu.vn>
2023-10-29 21:02:24 +0700

    add SC_Create but error line 188 in exception.cc

---
+++ NachOS-4.0/code/test/createfile.c
@@ -0,0 +1,18 @@
+#include "syscall.h"
+#define maxlen 32
+
+int main()
+{
+    int len;
+    char filename[maxlen + 1];
+    /*Create a file*/
+    if (Create("Text.txt") == -1)
+    {
+        // xuất thông báo lỗi tạo tập tin
+    }
+    else
+    {
+        // xuất thông báo tạo tập tin thành công
+    }
+    Halt();
+}
