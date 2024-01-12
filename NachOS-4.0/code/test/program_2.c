66a1bd2a1b4dab8c17aa9bebd0abe631229a4836
thaquan <thaquan@Ubuntu.myguest.virtualbox.org>
2024-01-12 18:54:23 +0700

    fix demo wait signal

--- NachOS-4.0/code/test/program_2.c
+++ NachOS-4.0/code/test/program_2.c
@@ -3,60 +3,22 @@
 
 int main()
 {
-    // int i;
-    // char*c;
-    // i=0;
-    // while(1)
-    // {
-    //     i=i+1;
-    //     if(i>=5)
-    //     {
-    //         Signal("program_1");
-    //         return;
-    //     }
-    //     c="b";
-    //     PrintConsole(c,1);
-        
-    // }
-    // Signal("program_1");
-
-    // Program copy
-
-    int len;
-    char a[] = "demo.txt";
-    char b[] = "demo_copy.txt";
-    int fileid_b;
-    int fileid_a;
-    int length = maxlen;
-    fileid_b = Create(b);
-    if (fileid_b == 1)
+    int i;
+    char*c;
+    i=0;
+    while(1)
     {
-        Signal("main");
-        return 0;
-    }
-    Close(fileid_b);
-    fileid_a = Open(a, 1);
-    fileid_b = Open(b, 1);
-    if (fileid_a != -1 &&fileid_b!=-1)
-    {
-        char *buffer = "0123456789";
-        int size = 10;
-        int size_buff = Read(buffer, size, fileid_a);
-        while (size_buff > 0)
+        i=i+1;
+        if(i>=5)
         {
-            if (size_buff != size)
-            {
-                Write(buffer, size_buff, fileid_b);
-                break;
-            }
-            Write(buffer, size, fileid_b);
-            size_buff = Read(buffer, size, fileid_a);
-            Signal("program_1");
-
+            Signal("pro1");
+            return;
         }
-        Close(fileid_b);
+        c="b";
+        PrintConsole(c,1);
+        
     }
-    Close(fileid_a);
-    Signal("program_1");
+    Signal("pro1");
+
     return 0;
 }
